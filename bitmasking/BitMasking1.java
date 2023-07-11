905ee41562b8bea64741f4d8a898b077d89f39d2
mgKang3646 <lordofkangs@naver.com>
2023-07-11 11:25:13 +0900

    //BOJ1285 동전뒤집기 BitMasking

---
+++ bitmasking/BitMasking1.java
@@ -0,0 +1,56 @@
+package org.example.bitmasking;
+
+import java.io.*;
+import static java.lang.Math.min;
+
+//BOJ1285 동전뒤집기
+public class BitMasking1 {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static int n;
+    static char[][] map;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+    }
+
+    public static void input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+        map = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+    }
+
+    public static void solution() throws IOException {
+        int ans = Integer.MAX_VALUE;
+        for(int bitCase = 0; bitCase<(1<<n);bitCase++){
+            int sum = 0;
+            for(int j=0;j<n;j++){
+                int count = 0;
+                for(int i=0;i<n;i++){
+                    char tmp = map[i][j];
+                    if(( bitCase & (1<<i) ) != 0) {
+                        tmp = reverse(tmp);
+                    }
+                    if(tmp == 'T')  count += 1;
+                }
+                sum += min(count,n-count); //열을 기준으로 뒤집을지 말지 결정한다.
+            }
+            ans = min(ans,sum);
+        }
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static char reverse(char value){
+        if( value=='T') return 'H';
+        else return 'T';
+    }
+}
