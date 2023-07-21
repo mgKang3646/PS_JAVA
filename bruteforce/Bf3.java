2c22abc638da7631bde081ce8ef5d74b688a0e63
mgKang3646 <lordofkangs@naver.com>
2023-07-21 11:40:32 +0900

    //BOJ15651 N과M(3)

---
+++ bruteforce/Bf3.java
@@ -0,0 +1,44 @@
+package org.example.bruteforce;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//BOJ15651 N과M(3)
+public class Bf3 {
+
+    static int n;
+    static int m;
+
+    static StringBuilder sb;
+
+    //static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        //visited = new boolean[n];
+
+        recursive(0,"");
+        System.out.println(sb.toString());
+
+
+    }
+
+    public static void recursive(int depth, String result){
+        if(depth == m) {
+            sb.append(result).append("\n");
+            return;
+        }
+        for(int i=1;i<=n;i++){
+            String tmp = result + i + " ";
+            recursive(depth+1,tmp);
+        }
+    }
+}
